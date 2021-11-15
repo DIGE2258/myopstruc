29249a123a6d7a97cd392e7a949ca943978f7841
DIGE2258 <729289872@qq.com>
2021-11-15 09:42:49 +0800

    lab05

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,6 @@
 #![feature(asm)]
 #![feature(global_asm)]
 #![feature(panic_info_message)]
-#![feature(alloc_error_handler)]
-
-extern crate alloc;
-
-#[macro_use]
-extern crate bitflags;
 
 #[macro_use]
 mod console;
@@ -20,8 +14,6 @@ mod loader;
 mod config;
 mod task;
 mod timer;
-mod sync;
-mod mm;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -31,27 +23,17 @@ fn clear_bss() {
         fn sbss();
         fn ebss();
     }
-    unsafe {
-        core::slice::from_raw_parts_mut(
-            sbss as usize as *mut u8,
-            ebss as usize - sbss as usize,
-        ).fill(0);
-    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    println!("[kernel] Hello, world!");
-    mm::init();
-    println!("[kernel] back to world!");
-    mm::remap_test();
-    task::add_initproc();
-    println!("after initproc!");
+    println!("[Kernel] Hello, world!");
     trap::init();
+    loader::load_apps();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
-    loader::list_apps();
-    task::run_tasks();
+    task::run_first_task();
     panic!("Unreachable in rust_main!");
 }
